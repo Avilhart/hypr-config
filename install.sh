894f3815b646e24a032e51b54d3537068bff1d0e
Avilhart <avilhart@gmail.com>
2025-03-04 14:00:50 +0200

    Add i3 configs

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S hyprland hyprpaper waybar firefox neovim alacritty mc cmake smplayer cmus wofi numlockx udiskie pamixer grim slurp feh zip unzip
+sudo pacman -S hyprland hyprpaper waybar i3 dmenu firefox neovim alacritty mc cmake smplayer cmus wofi numlockx udiskie pamixer grim slurp feh zip unzip
 
 echo "Installing fonts..."
 sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
 
 echo "Copying settings..."
 cp -r .config ~/
+cp -r .bash_profile ~/
 cp -r .bashrc ~/
+cp -r .xinitrc ~/
 
 echo "Volume 100%..."
 pamixer --set-volume 100
