0ea281165a0c142771c58aacb8472949e96740b9
Avilhart <avilhart@gmail.com>
2025-03-05 13:27:13 +0200

    Add bspwm & sxhkd config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing programs..."
-sudo pacman -S hyprland hyprpaper waybar i3 dmenu firefox neovim alacritty mc cmake smplayer cmus wofi numlockx udiskie pamixer grim slurp feh zip unzip
+sudo pacman -S hyprland hyprpaper waybar i3 bspwm sxhkd dmenu firefox neovim alacritty mc cmake smplayer cmus wofi picom polybar numlockx udiskie pamixer grim slurp feh zip unzip
 
 echo "Installing fonts..."
 sudo pacman -S ttf-liberation ttf-dejavu opendesktop-fonts ttf-bitstream-vera ttf-arphic-ukai ttf-arphic-uming ttf-hanazono ttf-jetbrains-mono-nerd
